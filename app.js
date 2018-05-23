ce867a3a8c4cad2be284d3398d19acd64500a563
Brian <briantechdom@gmail.com>
2018-05-23 20:25:28 +0000

    Adding addititional template files

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Setting view engine
 app.set("view engine", "ejs");
 
+var campgrounds = [
+        {name: "Salmon Creek", image: ""},
+        {name: "Granite Hill", image: ""},
+        {name: "Goat's rest", image: ""}
+    ]
+
 // Render function for the landing template
 app.get("/", function(req, res) {
     res.render("landing");
@@ -13,11 +19,7 @@ app.get("/", function(req, res) {
 
 // Routes to show all campgrounds taht are made
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-        {name: "Salmon Creek", image: ""},
-        {name: "Granite Hill", image: ""},
-        {name: "Goat's rest", image: ""}
-    ]
+    
     
     // render the campgrounds template
     res.render("campgrounds", {campgrounds:campgrounds});
@@ -25,9 +27,17 @@ app.get("/campgrounds", function(req, res) {
 
 // Allows user to post new campgrounds
 app.post("/campgrounds", function(req,res) {
-    res.send("You hit the post route!");
+    // res.send("You hit the post route!");
     // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    
+    //creates the new array indice item
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    
     //redirect back to campgrounds page
+    res.redirect("/campgrounds");
 });
 
 // rendering new form
