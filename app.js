375698db33df2364a0837946489846f1117c0ab7
Brian <briantechdom@gmail.com>
2018-05-23 17:12:26 +0000

    Added render and set functions

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var app = express();
 
+// Setting view engine
+app.set("view engine", "ejs");
 
+// Render function for the landing template
+app.get("/", function(req, res) {
+    res.render("landing");
+});
 
-
+// Routes to show all campgrounds taht are made
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name: "Salmon Creek", image: ""},
+        {name: "Granite Hill", image: ""},
+        {name: "Goat's rest", image: ""},
+    ]
+});
 
 app.get('/', (req, res) => res.send('Yelpcamp Server is running'));
 
